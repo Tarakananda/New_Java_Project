0dd59d59de3287df03dadc8bf0fef72f1d1d4f4b
Tarakananda <tharaknandha3@gmail.com>
2026-02-22 10:48:59 +0530

    first cmmit

---
+++ src/test/java/com/example/Java_Project/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
